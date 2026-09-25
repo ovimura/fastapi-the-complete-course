df3051daee46c20c97ad87c6cf88cc6d52b0fd1e
Ovidiu Mura <ovidiu.mura@yahoo.com>
2026-09-24 23:50:13 -0700

    load .env file in the environment

--- fastapidemo/TodoApp/alembic/env.py
+++ fastapidemo/TodoApp/alembic/env.py
@@ -7,6 +7,9 @@ from alembic import context
 
 import models
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
--- fastapidemo/TodoApp/database.py
+++ fastapidemo/TodoApp/database.py
@@ -1,7 +1,12 @@
+
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-import os
+
+load_dotenv()
 
 # SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
 SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL')
