b422d7b19155b24b1895323c8d99adc5210ce541
Ovidiu Mura <ovidiu.mura@yahoo.com>
2026-09-24 23:42:40 -0700

    move secrets to .env file and don't track the .env file

--- fastapidemo/TodoApp/database.py
+++ fastapidemo/TodoApp/database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
 # SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
-SQLALCHEMY_DATABASE_URL = 'postgresql://neondb_owner:npg_0S2zoWuOBswD@ep-polished-dew-arq4509h.c-4.us-west-2.aws.neon.tech/TodoApplicationDatabase?sslmode=require&channel_binding=require'
+SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL')
 
 # engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={
 #     'check_same_thread': False
--- fastapidemo/TodoApp/test/test_example.py
+++ fastapidemo/TodoApp/test/test_example.py
@@ -1,3 +1,13 @@
 def test_equal_or_not_equal():
     assert 1 == 1
     assert 1 != 2
+
+def test_is_instance():
+    assert isinstance(1, int)
+    assert isinstance('1', str)
+    assert not isinstance('1', int)
+
+def test_boolean():
+    validate = True
+    assert validate is True
+    assert ('hello' == 'hello') is True
