8f7e207b05bc84607aedb6fd703108f2482ae324
Ovidiu Mura <ovidiu.mura@yahoo.com>
2026-09-26 12:18:28 -0700

    adding auth tests for get current user invalid token unit test

--- fastapidemo/TodoApp/test/test_auth.py
+++ fastapidemo/TodoApp/test/test_auth.py
@@ -1,7 +1,9 @@
 from .utils import *
-from ..routers.auth import get_db, authenticate_user, create_access_token, SECRET_KEY, ALGORITHM
+from ..routers.auth import get_db, authenticate_user, create_access_token, SECRET_KEY, ALGORITHM, get_current_user
 from jose import jwt
 from datetime import timedelta
+from fastapi import HTTPException
+import pytest
 
 
 app.dependency_overrides[get_db] = override_get_db
@@ -32,8 +34,38 @@ def test_create_access_token(test_user):
                                 algorithms=[ALGORITHM], 
                                 options={"verify_signature": False})
     decoded_token_sub = decoded_token.get("sub")
-    decoded_token_user_id = decoded_token.get("user_id")
+    decoded_token_user_id = decoded_token.get("id")
     decoded_token_role = decoded_token.get("role")
     assert decoded_token_sub == username
     assert decoded_token_user_id == user_id
     assert decoded_token_role == role_user
+
+
+@pytest.mark.asyncio
+async def test_get_current_user_valid_token(test_user):
+    encode = {'sub': 'testuser', 'id': 1, 'role': 'admin'}
+    token = jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    user = await get_current_user(token)
+    assert user == {'username': 'testuser', 'id': 1, 'user_role': 'admin'}
+
+
+@pytest.mark.asyncio
+async def test_get_current_user_missing_payload(test_user):
+    encode = {'role': 'user'}
+    token = jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    with pytest.raises(HTTPException) as e:
+        await get_current_user(token)
+    assert e.value.status_code == status.HTTP_401_UNAUTHORIZED
+    assert e.value.detail == "Could not validate credentials"
+
+
+@pytest.mark.asyncio
+async def test_get_current_user_invalid_token(test_user):
+    token = "invalid_token"
+
+    with pytest.raises(HTTPException) as e:
+        await get_current_user(token)
+    assert e.value.status_code == status.HTTP_401_UNAUTHORIZED
+    assert e.value.detail == "Could not validate credentials"
